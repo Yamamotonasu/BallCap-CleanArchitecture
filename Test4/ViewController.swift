9af630075fca92ac82a0a470e548a65f093e8b60
Yamamotonasu <deep.blue.deep@outlook.jp>
2019-10-19 14:29:01 +0900

    first commit with .gitignore

---
+++ Test4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test4
+//
+//  Created by 山本裕太 on 2019/10/19.
+//  Copyright © 2019 山本裕太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
